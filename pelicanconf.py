413ad0c338dead5fa1aa21fe4a63eb00df26ae88
exell <andy@sussedit.com>
2013-06-24 23:37:21 +0100

    theme change?

--- pelicanconf.py
+++ pelicanconf.py
@@ -29,3 +29,6 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Specify name of a theme installed via the pelican-themes tool
+THEME = "gum"
