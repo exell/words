b56417985cdd20a753107cdc9bc133e890decae3
exell <andy@sussedit.com>
2013-06-29 20:44:40 +0000

    Update documentation

--- tipuedrop/tipuedrop_content.js
+++ tipuedrop/tipuedrop_content.js
@@ -1,7 +1,7 @@
 var tipuedrop = {"pages": [
      {"title": "Tipue drop", "thumb": "img/drop.png", "text": "Tipue drop, a search suggestion box jQuery plugin", "tags": "JavaScript js", "loc": "http://www.tipue.com/drop"},
      {"title": "Tipue Search", "thumb": "img/search.png", "text": "Tipue Search, a site search engine jQuery plugin", "tags": "JavaScript js", "loc": "http://www.tipue.com/search"}, 
-     {"title": "About Tipue", "thumb": "img/about.png", "text": "Tipue is a small web development studio based in North London", "tags": "", "loc": "http://www.tipue.com/about"}
-     {"title": "A beginning is a very delicate time",  "thumb": "tipuedrop/search.png/","text": "Yes, yes it is. Especially with all the fun I've had figuring out how to use github for the first time (bit behind eh?) and pelican for that matter, and finally, finally! It works! The first steps have been taken, a site has been made, now it's time to make it AWESOME. Or at least half decent. That is all for now.", "tags": "test", "loc": "http://exell.github.io/words/beginning.html"},
+     {"title": "About Tipue", "thumb": "img/about.png", "text": "Tipue is a small web development studio based in North London", "tags": "", "loc": "http://www.tipue.com/about"},
+     {"title": "A beginning is a very delicate time",  "thumb": "tipuedrop/search.png/","text": "Yes, yes it is. Especially with all the fun I've had figuring out how to use github for the first time (bit behind eh?) and pelican for that matter, and finally, finally! It works! The first steps have been taken, a site has been made, now it's time to make it AWESOME. Or at least half decent. That is all for now.", "tags": "test", "loc": "http://exell.github.io/words/beginning.html"}
 
 ]};
