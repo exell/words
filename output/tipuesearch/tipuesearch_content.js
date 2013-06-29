b91d8aaaf0cc36ec2dcdb631bd1ff07e25360c7c
exell <andy@sussedit.com>
2013-06-29 21:06:39 +0100

    sidebar-remove links,add search

--- output/tipuesearch/tipuesearch_content.js
+++
@@ -1,6 +0,0 @@
-var tipuesearch = {"pages": [
-     {"title": "A beginning is a very delicate time", "text": "Yes, yes it is. Especially with all the fun I've had figuring out how to use github for the first time (bit behind eh?) and pelican for that matter, and finally, finally! It works! The first steps have been taken, a site has been made, now it's time to make it AWESOME. Or at least half decent. That is all for now.", "tags": "", "": "http://exell.github.io/words/beginning.html"},
-]};
-
-
-
